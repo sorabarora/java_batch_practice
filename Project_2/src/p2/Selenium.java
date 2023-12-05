14066d3c6c28381abf37b2eb42636a0c14b86884
manu <manu@DESKTOP-FGJUHHM>
2023-12-05 13:39:43 +0530

    First Commit Done

---
+++ Project_2/src/p2/Selenium.java
@@ -0,0 +1,20 @@
+package p2;
+
+import p1.Automation;
+
+public class Selenium {
+	
+	public static void main(String[] args) {
+		
+		Automation a1=new Automation();
+		a1.method1();
+		a1.method2(10);
+		
+		
+		
+		
+		
+		
+	}
+
+}
